51286c7f715dd74e62295c664cf89a2467c20afb
ADMIN <ADMIN@LAPTOP-7OOPAIV7>
2022-09-04 12:49:48 +0530

    new ak code

---
+++ Akcode/src/org/sample/VehicleType.java
@@ -0,0 +1,11 @@
+package org.sample;
+
+public class VehicleType {
+	private void vehBrand() {
+		System.out.println("Maruthi");
+	}
+public static void main(String[]args){
+	VehicleType a = new VehicleType();
+	a.vehBrand();
+}
+}
---
+++ Akcode/src/org/sample/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ADMIN
+ *
+ */
+package org.sample;
